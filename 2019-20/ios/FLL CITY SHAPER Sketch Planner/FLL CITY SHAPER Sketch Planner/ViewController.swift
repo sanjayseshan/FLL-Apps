070e1c8d5f2b71a720a099193c5eb1ce9658a85e
Sanjay Seshan <sanjay@Sanjays-iMac.home>
2019-08-14 21:44:08 -0400

    ios sketcher initial

---
+++ 2019-20/ios/FLL CITY SHAPER Sketch Planner/FLL CITY SHAPER Sketch Planner/ViewController.swift
@@ -0,0 +1,157 @@
+//
+//  ViewController.swift
+//  FLL CITY SHAPER Sketch Planner
+//
+//  Created by Sanjay Seshan on 8/14/19.
+//  Copyright © 2019 ev3lessons.com. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+// Wrap the WKWebView webview to allow IB use
+
+class MyWebView : WKWebView {
+    required init?(coder: NSCoder) {
+        let configuration = WKWebViewConfiguration()
+        let controller = WKUserContentController()
+        configuration.userContentController = controller;
+        configuration.mediaTypesRequiringUserActionForPlayback = [];
+        super.init(frame: CGRect.zero, configuration: configuration)
+    }
+}
+
+class ViewController: UIViewController {
+    @IBOutlet weak var webView: MyWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let bounds = UIScreen.main.bounds
+        
+        webView.navigationDelegate = self;
+        webView.contentScaleFactor = bounds.size.width/100
+        webView.contentMode = UIView.ContentMode.scaleAspectFit
+        
+        switch UIDevice.current.userInterfaceIdiom {
+        case .phone:
+            //if let filePath = Bundle.main.path(forResource:"index-iphone", ofType:"html", inDirectory: "www") {
+            if let filePath = Bundle.main.path(forResource:"index", ofType:"html", inDirectory: "www") {
+
+                do {
+                    // load html string - baseURL needs to be set for local files to load correctly
+                    let html = try String(contentsOfFile: filePath, encoding: .utf8)
+                    webView.loadHTMLString(html, baseURL: Bundle.main.resourceURL?.appendingPathComponent("www"))
+                    
+                } catch {
+                    print("Error loading html")
+                }
+                //            }
+            }
+        // It's an iPhone
+        case .pad:
+            if let filePath = Bundle.main.path(forResource:"index", ofType:"html", inDirectory: "www") {
+                
+                do {
+                    // load html string - baseURL needs to be set for local files to load correctly
+                    let html = try String(contentsOfFile: filePath, encoding: .utf8)
+                    webView.loadHTMLString(html, baseURL: Bundle.main.resourceURL?.appendingPathComponent("www"))
+                    
+                } catch {
+                    print("Error loading html")
+                }
+                //            }
+            }
+        // It's an iPad
+        case .unspecified: break
+        // Uh, oh! What could it be?
+        case .tv:
+            break
+        case .carPlay:
+            break
+        @unknown default:
+            break
+        }
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func callJavascriptTapped(_ sender: UIButton) {
+        let script = "testJS()"
+        webView.evaluateJavaScript(script) { (result: Any?, error: Error?) in
+            if let error = error {
+                print("evaluateJavaScript error: \(error.localizedDescription)")
+            } else {
+                print("evaluateJavaScript result: \(result ?? "")")
+            }
+        }
+    }
+    func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo,
+                 completionHandler: @escaping () -> Void) {
+        
+        let alertController = UIAlertController(title: nil, message: message, preferredStyle: .actionSheet)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action) in
+            completionHandler()
+        }))
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    
+    func webView(_ webView: WKWebView, runJavaScriptConfirmPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo,
+                 completionHandler: @escaping (Bool) -> Void) {
+        
+        let alertController = UIAlertController(title: nil, message: message, preferredStyle: .actionSheet)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action) in
+            completionHandler(true)
+        }))
+        
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .default, handler: { (action) in
+            completionHandler(false)
+        }))
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    
+    func webView(_ webView: WKWebView, runJavaScriptTextInputPanelWithPrompt prompt: String, defaultText: String?, initiatedByFrame frame: WKFrameInfo,
+                 completionHandler: @escaping (String?) -> Void) {
+        
+        let alertController = UIAlertController(title: nil, message: prompt, preferredStyle: .actionSheet)
+        
+        alertController.addTextField { (textField) in
+            textField.text = defaultText
+        }
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action) in
+            if let text = alertController.textFields?.first?.text {
+                completionHandler(text)
+            } else {
+                completionHandler(defaultText)
+            }
+        }))
+        
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .default, handler: { (action) in
+            completionHandler(nil)
+        }))
+        
+        present(alertController, animated: true, completion: nil)
+    }
+}
+
+
+
+extension ViewController : WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        print("didFinish navigation:");
+    }
+}
+
+
+
+
---
+++ 2019-20/ios/FLL CITY SHAPER Sketch Planner/FLL CITY SHAPER Sketch PlannerTests/FLL_CITY_SHAPER_Sketch_PlannerTests.swift
@@ -0,0 +1,34 @@
+//
+//  FLL_CITY_SHAPER_Sketch_PlannerTests.swift
+//  FLL CITY SHAPER Sketch PlannerTests
+//
+//  Created by Sanjay Seshan on 8/14/19.
+//  Copyright © 2019 ev3lessons.com. All rights reserved.
+//
+
+import XCTest
+@testable import FLL_CITY_SHAPER_Sketch_Planner
+
+class FLL_CITY_SHAPER_Sketch_PlannerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
